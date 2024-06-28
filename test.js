899ff33b1053289b165ad96079e55904327713bf
MKPLKN <15703708+MKPLKN@users.noreply.github.com>
2024-06-28 08:00:34 +0300

    Switch to brittle

--- test.js
+++ test.js
@@ -1,26 +1,23 @@
-const tape = require('tape')
+const test = require('brittle')
 const seed = require('./')
 
-tape('returns a number', function (t) {
+test('returns a number', function (t) {
   const random = seed()
 
   const b = random()
-  t.same(typeof b, 'number')
-  t.end()
+  t.is(typeof b, 'number')
 })
 
-tape('is seedable', function (t) {
+test('is seedable', function (t) {
   let random = seed()
 
   const b1 = random()
-  t.same(typeof b1, 'number')
+  t.is(typeof b1, 'number')
 
   const b2 = random()
-  t.same(typeof b2, 'number')
+  t.is(typeof b2, 'number')
 
   random = seed(random.seed)
-  t.same(b1, random())
-  t.same(b2, random())
-
-  t.end()
+  t.is(b1, random())
+  t.is(b2, random())
 })
